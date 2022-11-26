02b55268b58c9fa814ecd612a0aef2395ffbc3fd
Elena Blagodir <elenalx28@gmail.com>
2022-11-26 23:47:32 +0200

    2

--- src/js/01-gallery.js
+++ src/js/01-gallery.js
@@ -1,15 +1,40 @@
 import { galleryItems } from './gallery-items.js';
 // Change code below this line
 
+
 const instance = basicLightbox.create(`
     <div class="modal">
       <img class="gallery_image"
       src='https://cdn.pixabay.com/photo/2016/11/18/16/19/flowers-1835619_1280.jpg',
       alt="flower"/>
-    </div>
-`)
+    </div>`)
+
+const galleryContainer = document.querySelector("div.gallery");
+const galleryGreate = makeCreateGallery(galleryItems);
+
+galleryContainer.insertAdjacentHTML('beforeend',galleryGreate);
+
+
+function makeCreateGallery(galleryItems ) {
+  return galleryItems.map(({ preview, description, original }) => {
+    return 
+     `<div class="gallery__item">
+      <a class="gallery__link" href="${original}">
+        <img class="gallery__image"
+          src="${preview}"
+          data-source="${original}"
+          alt="${description}"
+    />
+  </a>
+</div>`;
+  })
+  .join("");
+}
 
 
 
-instance.show();
-console.log(galleryItems);
+
+
+
+// instance.show();
+// console.log(galleryItems);
